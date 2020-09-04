56cf4f57c6db83caa23d54647647f1b958b06ee3
Julia Offerman <59590610+juliaofferman@users.noreply.github.com>
2020-09-03 18:23:44 -0700

    Julia floating point (#1)
    * Add list generator to TestGen.ml
    
    * Add rev and list equality components
    
    * Add real number type
    
    * Add real number value
    
    * Create RealComponents.ml
    
    * change can_apply to is_argument_valid
    
    * Update RealComponents.ml
    
    * Update RealComponents.ml
    
    * Update RealComponents.ml
    
    * Update RealComponents.ml
    
    * Update RealComponents.ml
    
    * Add Real components
    
    * Add real type
    
    * Additional job examples
    
    * Update TestGen.ml
    
    * Update TestGen.ml
    
    * Update real constants
    
    * Update Real component to_string's
    
    * Update TestGen.ml
    
    * Update App.ml
    
    * Update App.ml
    
    * Tests for real components
    
    * Update to include real component tests
    
    * Update Test_RealComponents.ml
    
    * Update Test_RealComponents.ml
    
    * Update Test_RealComponents.ml
    
    * Add additional tests
    
    * Fix negation
    
    * Update Value.ml
    
    * remove real-ite component
    
    * Add tests for PIE involving the Real type
    
    * Remove 'ite' component tests
    
    * Remove redundant tests

--- test/Test_PIE.ml
+++ test/Test_PIE.ml
@@ -76,10 +76,53 @@ let abs_zero_initial_features () =
     (List.map [(-1) ; 3 ; 0 ; (-2) ; 6] ~f:(fun i -> [Value.Int i])))
   in Alcotest.(check string) "identical" "(>= x 0)" (cnf_opt_to_desc res)
 
+
+ let real_abs_job = Job.create_unlabeled
+  ~f:(fun [@warning "-8"] [ Value.Real x ] -> Value.Real (if Float.(x > 0.) then x else (-1. *. x)))
+  ~args:([ ("x", Type.REAL) ])
+  ~post:(fun inp res ->
+          match [@warning "-8"] inp , res with
+          | [ Value.Real x ], Ok (Value.Real y) -> Float.(equal x y))
+  ~features:[
+    ((fun [@warning "-8"] [Value.Real x] -> Float.(x > 0.)), "(> x 0.)")
+  ]
+  (List.map [(-1.4) ; 3.5 ; 0. ; (-2.6) ; 6.0] ~f:(fun i -> [Value.Real i]))
+
+
+ let real_abs_precond_auto_1 () =
+  let (res, _) = learnPreCond (Job.add_feature ~job:real_abs_job
+                                 ((fun [@warning "-8"] [Value.Real x] -> Float.(equal (x +. x) x)),
+                                 "(= x (+ x x))"))
+                               ~config:{ PIE.Config.default with
+                                        _BFL = {
+                                          BFL.Config.default with
+                                          min_clauses = 1
+                                        ; auto_increment_clauses = true
+                                        }
+                                      ; disable_synth = true
+                                      }
+  in Alcotest.(check string) "identical" "(or (= x (+ x x)) (> x 0.))"
+                             (cnf_opt_to_desc res)
+
+let real_abs_zero_initial_features () = 
+ let (res, _) = learnPreCond (Job.create_unlabeled
+  ~f:(fun [@warning "-8"] [ Value.Real x ] -> Value.Real (if Float.(x > 0.) then x else (-1. *. x)))
+  ~args:([ ("x", Type.REAL) ])
+  ~post:(fun inp res ->
+          match [@warning "-8"] inp , res with
+          | [ Value.Real x ], Ok (Value.Real y) -> Float.(equal x y))
+  ~features:[]
+  (List.map [(-1.4) ; 3.5 ; 0. ; (-2.6) ; 6.0] ~f:(fun i -> [Value.Real i])))
+    in Alcotest.(check string) "identical" "(>= x 0.)" (cnf_opt_to_desc res)
+
+
+
 let all = [
-  "Abs Conflict Failure",    `Quick, abs_conflict_failure  ;
-  "Abs Conflict Group",      `Quick, abs_conflict_group    ;
-  "Abs Precond 1-CNF",       `Quick, abs_precond_1_cnf ;
-  "Abs Precond Auto >= 1",   `Quick, abs_precond_auto_1 ;
-  "Abs No Initial Features", `Quick, abs_zero_initial_features ;
+  "Abs Conflict Failure",         `Quick, abs_conflict_failure  ;
+  "Abs Conflict Group",           `Quick, abs_conflict_group    ;
+  "Abs Precond 1-CNF",            `Quick, abs_precond_1_cnf ;
+  "Abs Precond Auto >= 1",        `Quick, abs_precond_auto_1 ;
+  "Abs No Initial Features",      `Quick, abs_zero_initial_features ;
+  "Real Abs Precond Auto >= 1",   `Quick, real_abs_precond_auto_1 ;
+  "Real Abs No Initial Features", `Quick, real_abs_zero_initial_features ;
 ]
